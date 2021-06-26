2f1cdfc1d3b6dcb2ed624f44ccf5e81ce56a1db7
nickumia <nickumia6@gmail.com>
2021-06-26 18:30:28 -0400

    embed source selection in function arguments

--- app/gather.py
+++ app/gather.py
@@ -1,15 +1,13 @@
 ##  Run scrapy to pull quote
 #   Developer: nickumia
 
-global IMG
 
 import json
 import os
 
-IMG = True
 
-def get_quote():
-    if not IMG:
+def get_quote(img=True):
+    if img != "img":
         os.system("sed -i 's/https:\/\/www.insightoftheday.com/https:\/\/quotes.toscrape.com/' /app/gather_quotes/spiders/quotes_spider.py")
     else:
         os.system("sed -i 's/https:\/\/quotes.toscrape.com/https:\/\/www.insightoftheday.com/' /app/gather_quotes/spiders/quotes_spider.py")
