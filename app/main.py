f98a6865d86dd96a0356234250c286920ab29fde
nickumia <nickumia6@gmail.com>
2021-06-25 22:25:42 -0400

    basic setup of main application and building process

---
+++ app/main.py
@@ -0,0 +1,14 @@
+##	Quote-scraper and sender
+#	Developer: nickumia
+
+
+from datetime import datetime
+
+#import gather
+#import sender
+
+
+if __name__=="__main__":
+	current_time = datetime.now()
+	print(current_time)
+	print(type(current_time))
